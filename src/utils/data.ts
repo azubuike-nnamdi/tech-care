36fbe5935afd65731157bfa3910ebebbd0efe80c
iamclement1 <iamnnamdiclement@gmail.com>
2024-05-27 08:52:12 +0100

    chore: temp data :green_heart:

---
+++ src/utils/data.ts
@@ -0,0 +1,56 @@
+export const NavLink = [
+  {
+    id: 1,
+    icon: '/img/home_FILL0_wght300_GRAD0_opsz24.svg',
+    name: 'Overview',
+    path: '#',
+  },
+  {
+    id: 2,
+    icon: '/img/group_FILL0_wght300_GRAD0_opsz24.svg',
+    name: 'Patients',
+    path: '#',
+  },
+  {
+    id: 3,
+    icon: '/img/calendar_today_FILL0_wght300_GRAD0_opsz24.svg',
+    name: 'Schedule',
+    path: '#',
+  },
+  {
+    id: 4,
+    icon: '/img/chat_bubble_FILL0_wght300_GRAD0_opsz24.svg',
+    name: 'Profile',
+    path: '#',
+  },
+  {
+    id: 5,
+    icon: '/img/credit_card_FILL0_wght300_GRAD0_opsz24.svg',
+    name: 'Transactions',
+    path: '#',
+  }
+]
+
+export const Patients = [
+  {
+    id: 1,
+    name: 'Emily William',
+    gender: 'Female',
+    age: '18',
+    img: '/img/emily.png',
+  },
+  {
+    id: 2,
+    name: 'Ryan Johnson',
+    gender: 'Male',
+    age: '45',
+    img: '/img/ryan.png',
+  },
+  {
+    id: 3,
+    name: 'Brandon Mitchell',
+    gender: 'Male',
+    age: '36',
+    img: '/img/brandon.png',
+  }
+];

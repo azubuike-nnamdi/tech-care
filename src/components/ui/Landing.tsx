0b0c331859568cc657165513a085a07845b31b5e
iamclement1 <iamnnamdiclement@gmail.com>
2024-05-27 09:59:04 +0100

    chore: config landing :green_heart:

---
+++ src/components/ui/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Sidebar } from '../common/Sidebar'
+
+export const Landing = () => {
+  return (
+    <div className='flex items-center justify-between'>
+      <Sidebar />
+
+    </div>
+  )
+}

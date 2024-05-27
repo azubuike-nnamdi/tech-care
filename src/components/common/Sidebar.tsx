263cab6d4e4a409972b79ceb00b05b3dba193d8a
iamclement1 <iamnnamdiclement@gmail.com>
2024-05-27 10:39:37 +0100

    chore: instlal shadcn card component :green_heart

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 export const Sidebar = () => {
   return (
     <div className='bg-[#FFFFFF] p-3 w-[367px] rounded-lg md:overflow-y-auto h-screen'>
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center px-2">
         <h1 className='text-2xl font-bold'>Patients</h1>
         <Image src="/img/search_FILL0_wght300_GRAD0_opsz24.svg" alt='search_icon'
           width={15}
@@ -17,8 +17,8 @@ export const Sidebar = () => {
         {
           Patients.map((patient) => (
             <div key={patient.id}
-              className={`flex items-center justify-between p-4 rounded-lg ${patient.name === 'Jessica Taylor' && 'bg-[#D8FCF7] items-center'}`}>
-              <div className='flex space-x-4 pt-8'>
+              className={`flex items-center justify-between px-2 py-4 rounded-lg ${patient.name === 'Jessica Taylor' && 'bg-[#D8FCF7] items-center'}`}>
+              <div className='flex space-x-4'>
                 <Image src={patient.img} alt='patient' width={50} height={50} />
                 <div className='leading-1'>
                   <p className='text-[14px] font-bold'>{patient.name}</p>
